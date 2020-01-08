9fbd219e0726d436e44f056319bcc5cd1cde629b
Peter L Gunter Jr <petlgunjr@gmail.com>
2020-01-08 10:50:28 -0800

    basic chrome ext

---
+++ build/precache-manifest.258f57216b65c29dfa39c5d88284f36f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6482d1fe17dffb92e3bc9f9359e13150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "307cb79c522ed30595df",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fabc970ca3caa6eeb17f",
+    "url": "/static/js/2.749247d4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.749247d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "307cb79c522ed30595df",
+    "url": "/static/js/main.79f07cf5.chunk.js"
+  },
+  {
+    "revision": "c3e58fd10582d043c495",
+    "url": "/static/js/runtime-main.ee369ca8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
